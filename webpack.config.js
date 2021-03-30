17dd885fc9baf07e9caf9ee74780b51d1cb4fa53
Mike Franklin <293276+mikeemoo@users.noreply.github.com>
2021-03-30 01:15:37 +0000

    moved a few files around

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  context: __dirname,
+  entry: './src/server/server.ts',
+  output: {
+    path: path.resolve(
+      __dirname,
+      'build'
+     ),
+    filename: 'server.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  target: 'node',
+  externals: [
+    nodeExternals({
+      modulesDir: path.resolve(__dirname, "node_modules"),
+      additionalModuleDirs: [
+        path.resolve(__dirname, "..", "..", "node_modules")
+      ]
+    })
+  ],
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ }
+    ],
+  },
+};
